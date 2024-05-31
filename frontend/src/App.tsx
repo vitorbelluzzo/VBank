9cfca096900011d89f8f61e649842b69ecaa0d0d
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-05-31 03:00:15 -0300

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-import { Card } from "./components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input"
+import { 
+  Button,
+  buttonVariants 
+ } from "@/components/ui/button"
+
+
+
 
 export function App() {
   return (
-      <div >
-       <Card>
-       <h1 >Faça o login</h1>
-        <input placeholder="Seu Email" />
-        <input placeholder="Senha" />
-        <button >
-          Entrar
-        </button>
-       </Card>
-      </div>
-      
+    <div className="max-w-64 mx-auto mt-60">
+      <Card className=" text-center  bg-zinc-900 text-white border-none">
+        <CardHeader >
+          <CardTitle>Login</CardTitle>
+          
+        </CardHeader>
+        <CardContent className="flex flex-col gap-3  ">
+        <Input type="email"  placeholder="Email Address" className="bg-zinc-950 border-zinc-700 placeholder:text-zinc-600" />
+        <Input type="password"  placeholder="Senha" className="bg-zinc-950 border-zinc-700 placeholder:text-zinc-600" />
+        </CardContent>
+        <CardFooter className="flex justify-center">
+        <Button  className="bg-transparent border w-full  bg-blue-600 hover:bg-blue-500 border-none   ">Logar</Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
