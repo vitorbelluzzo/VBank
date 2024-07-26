c32fffc7d6bd865ef38238eaa4cd5a5519d0da7d
vitorbelluzzom <vitorbelluzzo@hotmail.com>
2024-07-25 23:55:29 -0300

    alteração do caminho da  utils

--- frontend/src/components/ui/skeleton.tsx
+++ frontend/src/components/ui/skeleton.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/services/lib/utils"
+import { cn } from "@/lib/utils"
 
 function Skeleton({
   className,
@@ -6,7 +6,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn("animate-pulse rounded-md bg-zinc-100 dark:bg-zinc-800", className)}
       {...props}
     />
   )
